583f4363eaae584c40e2bc227bd39e3bb13e97bf
Paulo Roberto <prmorais1302@gmail.com>
2020-12-15 21:34:59 -0300

    Criando o formulario HTML

--- src/ConversorMoedas.tsx
+++ src/ConversorMoedas.tsx
@@ -1,8 +1,45 @@
 import './ConversorMoedas.css';
 
+import { Button, Col, Form, Jumbotron, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
 const ConversorMoedas = () => {
   return (
-    <h1>Conversor Moedas</h1>
+    <div>
+      <h1>Conversor Moedas</h1>
+      <Jumbotron>
+        <Form>
+          <Form.Row>
+            <Col sm="3">
+              <Form.Control value={1} required placeholder="0" />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+              </Form.Control>
+            </Col>
+
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+
+            <Col sm="3">
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Col>
+
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm" />
+                Converter
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+      </Jumbotron>
+
+    </div>
   );
 };
 
